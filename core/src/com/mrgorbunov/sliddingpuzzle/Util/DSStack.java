4328558513fb5957793d2e7ec5230acf75a5e559
MrGorbunov <volarcko@gmail.com>
2021-08-14 21:09:53 -0400

    Built Stack Class based on Gdx.utils.Array

---
+++ core/src/com/mrgorbunov/sliddingpuzzle/Util/DSStack.java
@@ -0,0 +1,51 @@
+package com.mrgorbunov.sliddingpuzzle.Util;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.Array;
+
+/**
+ * Stack Implementation, using Gdx.Array under the hood.
+ * 
+ * Stack is prefixed with DS to mean Data Structure Stack,
+ * and avoid naming conflict with the GDX UI stack & with
+ * java.util stack.
+ */
+
+public class DSStack<T> {
+
+	Array<T> arr;
+
+	public DSStack () {
+		arr = new Array<>();
+		Gdx.app.log("StackCommands", "Made new stack");
+	}
+
+	public int size () {
+		return arr.size;
+	}
+
+	/**
+	 * Returns the top of the stack without
+	 * popping it.
+	 * 
+	 * Returns null if the Stack is empty
+	 */
+	public T peekTop () {
+		Gdx.app.log("StackCommands", "Peeked at top with size = " + size());
+		if (arr.size == 0)
+			return null;
+
+		return arr.get(arr.size - 1);
+	}
+
+	public T pop () {
+		Gdx.app.log("StackCommands", "Popped stack");
+		return arr.pop();
+	}
+
+	public void add (T elm) {
+		Gdx.app.log("StackCommands", "Added element: " + elm);
+		arr.add(elm);
+	}
+	
+}
